d2a17c969e146c37ec5e4dda7afc9430aabdcfaf
Tshireletso <tmogwere09@gmail.com>
2024-06-09 19:13:18 +0200

    updates hosst file

--- Van life Route/src/pages/Host/HostVanInfo.jsx
+++ Van life Route/src/pages/Host/HostVanInfo.jsx
@@ -1,8 +1,15 @@
 import React from "react"
 
 function HostVanInfo() {
+    const { currentVan } = useOutletContext()
+    
     return (
-        <h2>Detailed info goes here</h2>
+        <section>
+            <h4>Name: {currentVan.name}</h4>
+            <h4>Category: {currentVan.type}</h4>
+            <h4>Description: {currentVan.description}</h4>
+            <h4>Visibility: public</h4>
+        </section>
     )
 }
 
