1a0ff49c723df29559ca09707db1e6a08bfbe28f
Tshireletso <tmogwere09@gmail.com>
2024-06-09 20:17:27 +0200

    function fake logout

--- Van life Route/components/Header.jsx
+++ Van life Route/components/Header.jsx
@@ -7,6 +7,10 @@ function Header() {
     textDecoration: "underline",
     color: "#161616"
 }
+
+function fakeLogOut() {
+  localStorage.removeItem("loggedin")
+}
 return (
 <header>
       <Link className="site-logo" to="/">#VANLIFE</Link>
