76be7b2c3460ed7adfe5a6b8f8014684ccfe3179
Tshireletso <tmogwere09@gmail.com>
2024-06-09 19:50:27 +0200

    login function

--- Van life Route/src/pages/Host/Login.jsx
+++ Van life Route/src/pages/Host/Login.jsx
@@ -2,3 +2,17 @@ import React from "react"
 import { useLocation, useNavigate } from "react-router-dom"
 import { loginUser } from "../api"
 
+function Login() {
+    const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" })
+    const [status, setStatus] = React.useState("idle")
+    const [error, setError] = React.useState(null)
+
+    const location = useLocation()
+    const navigate = useNavigate()
+    const from = location.state?.from || "/host";
+
+
+
+}
+
+export default Login
