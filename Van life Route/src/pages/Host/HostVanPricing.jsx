60223cd7f3b6f8ec6f6f7598e39fe77b383adec4
Tshireletso <tmogwere09@gmail.com>
2024-06-09 19:11:53 +0200

    updates import

--- Van life Route/src/pages/Host/HostVanPricing.jsx
+++ Van life Route/src/pages/Host/HostVanPricing.jsx
@@ -1,6 +1,8 @@
 import React from "react"
+import { useOutletContext } from "react-router-dom"
 
 function HostVanPricing() {
+    const { currentVan } = useOutletContext()
     return (
         <h2>Pricing info goes here</h2>
     )
