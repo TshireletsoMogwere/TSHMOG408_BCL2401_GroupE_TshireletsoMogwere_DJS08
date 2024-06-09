88f5c45b6e549fee88c0112e9bebd89622e15261
Tshireletso <tmogwere09@gmail.com>
2024-06-09 22:11:46 +0200

    modified code

--- Van life Route/src/pages/Host/HostVanDetail.jsx
+++ Van life Route/src/pages/Host/HostVanDetail.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { useParams, Link, NavLink, Outlet } from "react-router-dom"
-// import { getHostVans } from "../../api"
+import { getHostVans } from "../../api"
 
 function HostVanDetail() {
     const [currentVan, setCurrentVan] = React.useState(null)
