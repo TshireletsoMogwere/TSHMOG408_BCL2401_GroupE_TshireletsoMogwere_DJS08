a9349745e521ef21084452f381a50f7915852364
Tshireletso <tmogwere09@gmail.com>
2024-06-09 20:40:57 +0200

    updated missing span

--- Van life Route/src/pages/Host/HostVanInfo.jsx
+++ Van life Route/src/pages/Host/HostVanInfo.jsx
@@ -9,7 +9,7 @@ function HostVanInfo() {
             <h4>Name: <span>{currentVan.name}</span></h4>
             <h4>Category: <span>{currentVan.type}</span></h4>
             <h4>Description: <span>{currentVan.description}</span></h4>
-            <h4>Visibility: public</h4>
+            <h4>Visibility: <span>public</span></h4>
         </section>
     )
 }
