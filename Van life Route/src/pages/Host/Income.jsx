4cc72045734b4c9990b315b299a6462382791247
Tshireletso <tmogwere09@gmail.com>
2024-06-09 20:44:36 +0200

    array of transaction data

--- Van life Route/src/pages/Host/Income.jsx
+++ Van life Route/src/pages/Host/Income.jsx
@@ -2,6 +2,12 @@ import React from "react"
 import { Link } from "react-router-dom"
 
 function Income() {
+
+    const transactionsData = [
+        { amount: 720, date: "Jan 3, '23", id: "1" },
+        { amount: 560, date: "Dec 12, '22", id: "2" },
+        { amount: 980, date: "Dec 3, '22", id: "3" },
+    ]
     return (
         <>
         <h1>Income page goes here</h1>
