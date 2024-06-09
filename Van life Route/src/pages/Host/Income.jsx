d2e627faccd649c0decee9d8c4e2abf17ad85e13
Tshireletso <tmogwere09@gmail.com>
2024-06-09 18:34:28 +0200

    updates income file

--- Van life Route/src/pages/Host/Income.jsx
+++ Van life Route/src/pages/Host/Income.jsx
@@ -1,8 +1,12 @@
-
+import React from "react"
+import { Link } from "react-router-dom"
 
 function Income() {
     return (
+        <>
         <h1>Income page goes here</h1>
+        <Link to="something">Click me</Link>
+    </>
     )
 }
 
